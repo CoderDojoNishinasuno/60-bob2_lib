da6d0cfec7b69bad8c434fe5bb3a8e45e89c54ed
CoderDojo西那須野 <131562680+CoderDojoNishinasuno@users.noreply.github.com>
2025-02-06 23:16:13 +0900

    カスタムブロックを使うようにした。アルゴは変更なし。

--- main.ts
+++ main.ts
@@ -1,76 +1,80 @@
-function とめる () {
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-}
-function みぎて (数値: number) {
-    servos.P1.setAngle(60 - 数値)
-}
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-    	
-    } else if (receivedNumber == 2) {
-        まえにすすむ()
-    } else if (receivedNumber == 3) {
-        みぎにまわる(1)
-    } else if (receivedNumber == 4) {
-        ひだりにまわる(1)
-    } else if (receivedNumber == 5) {
-        basic.showIcon(IconNames.Square)
-    } else if (receivedNumber == 6) {
-        basic.showIcon(IconNames.StickFigure)
-    } else {
-        とめる()
+namespace BOB2 {
+
+    //% block="見せる|number $v"
+    export function showNumber(v: number, interval: number = 150)
+    {return(v)}
+
+    //% block="とめる"
+    export function stop() {
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 0)
     }
-})
-function ひだりて (数値: number) {
-    servos.P2.setAngle(120 - 数値)
-}
-function うしろにすすむ () {
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-}
-function まえにすすむ () {
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-}
-function みぎにまわる (じかん: number) {
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.pause(じかん)
-    とめる()
-}
-function ひだりてをふる (かいすう: number) {
-    for (let index = 0; index < かいすう; index++) {
-        ひだりて(-30)
-        basic.pause(200)
-        ひだりて(30)
-        basic.pause(200)
+
+    //% block="みぎて|number $angle"
+    export function rightHand(angle: number) {
+        servos.P1.setAngle(60 - angle)
+    }
+
+    //% block="ひだりて|number $angle"
+    export function leftHand(angle: number) {
+        servos.P2.setAngle(120 - angle)
+    }
+
+    //% block="うしろにすすむ"
+    export function backword() {
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
+
+    //% block="まえにすすむ"
+    export function forward() {
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+    }
+
+    //% block="みぎまわる|number $angle"
+    export function turnRight(t: number) {
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        basic.pause(t)
+        stop()
+    }
+
+    //% block="ひだりてふる|number $num"
+    export function waveLeftHand(num: number) {
+        for (let index = 0; index < num; index++) {
+            leftHand(-30)
+            basic.pause(200)
+            leftHand(30)
+            basic.pause(200)
+        }
+    }
+
+    //% block="ひだりまわる| number $t"
+    export function turnLeft(t: number) {
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(t)
+        stop()
+    }
+
+    //% block="みぎてふる|number $num"
+    export function waveRightHand(num: number) {
+        for (let index2 = 0; index2 < num; index2++) {
+            rightHand(-30)
+            basic.pause(200)
+            rightHand(30)
+            basic.pause(200)
+        }
     }
 }
-function ひだりにまわる (じかん: number) {
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    basic.pause(じかん)
-    とめる()
-}
-function みぎてをふる (かいすう: number) {
-    for (let index = 0; index < かいすう; index++) {
-        みぎて(-30)
-        basic.pause(200)
-        みぎて(30)
-        basic.pause(200)
-    }
-}
-basic.forever(function () {
-	
-})
